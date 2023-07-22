b4e264eeaf384f70977f44058b980170110fcb3e
Bruno Costa <brunoscosta17@gmail.com>
2023-07-22 02:50:12 -0300

    finalizado banner

--- src/app/components/landing-page/banner/banner.component.ts
+++ src/app/components/landing-page/banner/banner.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
+
+  faPhoneFlip = faPhoneFlip;
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
+  faWhatsapp = faWhatsapp;
 
   constructor() { }
 
