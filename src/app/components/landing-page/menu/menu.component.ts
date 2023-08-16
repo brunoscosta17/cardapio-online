3cce66d9316f2a34a537ba27541b31d8adff5e16
Bruno Costa <brunoscosta17@gmail.com>
2023-08-16 13:50:04 -0300

    Finished Menu Component

--- src/app/components/landing-page/menu/menu.component.ts
+++ src/app/components/landing-page/menu/menu.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  faHamburger,
+  faPizzaSlice,
+  faDrumstickBite,
+  faBacon,
+  faCocktail,
+  faIceCream,
+  faMinus,
+  faPlus,
+  faShoppingBag,
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,9 +19,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  faMinus = faMinus;
+  faPlus = faPlus;
+  faShoppingBag = faShoppingBag;
+
+  menuItems: Array<any> = [
+    { icon: faHamburger, text: 'Burgers', active: true },
+    { icon: faPizzaSlice, text: 'Pizzas' },
+    { icon: faDrumstickBite, text: 'Churrasco' },
+    { icon: faBacon, text: 'Steaks' },
+    { icon: faCocktail, text: 'Bebidas' },
+    { icon: faIceCream, text: 'Sobremesas' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleService(event: any): void {
+    console.log(event)
+    this.menuItems.forEach((item, index) => {
+      item.active = false
+      if (item.text === event.text) {
+        this.menuItems[index].active = true;
+      }
+    });
   }
 
 }
