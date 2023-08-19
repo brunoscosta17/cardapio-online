bdf5131f393f75f0694d05136a36814af2e2747e
Bruno Costa <brunoscosta17@gmail.com>
2023-08-19 18:54:13 -0300

    Finished Testimonials Component

--- src/app/components/landing-page/testimonials/testimonials.component.ts
+++ src/app/components/landing-page/testimonials/testimonials.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faStar, faStarHalfAlt, faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
   styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
+
+  faStar = faStar;
+  faStarHalfAlt = faStarHalfAlt;
+  faQuoteLeft = faQuoteLeft;
+  faQuoteRight = faQuoteRight;
+  farStar = farStar;
 
   constructor() { }
 
